01b8f8efbd78564b8621dea4c06938f776ba755b
zz` <2536026346@qq.com>
2025-08-15 14:17:07 +0800

    系统优化 --AI并发调用√ --Redis缓存优化√

--- src/main/java/org/example/aigeneration/controller/AppController.java
+++ src/main/java/org/example/aigeneration/controller/AppController.java
@@ -8,7 +8,6 @@ import com.mybatisflex.core.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.example.aigeneration.ai.AiCodeGenTypeRoutingService;
 import org.example.aigeneration.annotation.AuthCheck;
 import org.example.aigeneration.common.BaseResponse;
 import org.example.aigeneration.common.DeleteRequest;
@@ -25,6 +24,7 @@ import org.example.aigeneration.model.vo.AppVO;
 import org.example.aigeneration.service.AppService;
 import org.example.aigeneration.service.ProjectDownloadService;
 import org.example.aigeneration.service.UserService;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
@@ -233,6 +233,11 @@ public class AppController{
      * @return 精选应用列表
      */
     @PostMapping ("/good/list/page/vo")
+    @Cacheable (
+            value = "good_app_page",
+            key = "T(org.example.aigeneration.utils.CacheKeyUtils).generateKey(#appQueryRequest)",
+            condition = "#appQueryRequest.pageNum <= 10"
+    )
     public BaseResponse<Page<AppVO>> listGoodAppVOByPage(@RequestBody AppQueryRequest appQueryRequest){
         ThrowUtils.throwIf(appQueryRequest==null, ErrorCode.PARAMS_ERROR);
         // 限制每页最多 20 个
